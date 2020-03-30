e1c4a3ecaba95392e9d888c5580ced3ff0d330eb
manornot <ormanis1609@gmail.com>
2020-03-30 09:50:42 +0300

    no thread listener

--- Gates/listener_mqtt_v1.2.py
+++ Gates/listener_mqtt_v1.2.py
@@ -5,7 +5,7 @@ import numpy as np
 import RPi.GPIO as gpio
 import paho.mqtt.client as client
 import paho.mqtt.publish
-import threading
+#import threading
 import csv
 fields = ['M00','MO1','M10','M11','time']
 from fsm import gates_FSM
@@ -17,17 +17,17 @@ gates = gates_FSM()
 available_commands = [b'open',b'stop',b'close']
 
 def open_command(gate):
-    #print('Roger Roger! Execute openning protocol!')
+    if DEBUG: print('DEBUG','Roger Roger! Execute openning protocol!')
     #time.sleep(1)
-    print(gate.state)
+    if DEBUG: print('DEBUG',gate.state)
     if gate.state == 'close' or gate.state == 'stop':
-        print('push me!')
+        if DEBUG: print('DEBUG','push me!')
         button_press()
 def close_command(gate):    
     if gate.state == 'open':
            button_press()
     elif gate.state == 'stop':
-        print(gate.prev_state)
+        if DEBUG: print('DEBUG',gate.prev_state)
         if gate.prev_state == 'opening':
             button_press()
         elif gate.prev_state == 'closing':
@@ -44,7 +44,7 @@ def stop_command(gate):
    		
 def button_press():
     #pass
-    print('open this fucking doors')
+    if DEBUG: print('DEBUG','open this fucking doors')
     gpio.output(11,1)
     time.sleep(1)
     gpio.output(11,0)
@@ -61,17 +61,17 @@ def main(gates):
 
 
 def on_connect(client, userdata, flags, rc):
-    print("i'm in\n")
+    if DEBUG: print('DEBUG',"i'm in\n")
     sys.stdout.flush()
     client.disconnect_flag = False
     client.subscribe(topic='gates/command')
 
 def on_message(client, userdata, message):
-    print('message received=',message.payload,time.time()) 
-    #print(userdata.get('gates').state)
+    if DEBUG: print('DEBUG','message received=',message.payload,time.time()) 
+    if DEBUG: print('DEBUG',userdata.get('gates').state)
     sys.stdout.flush()
     if(any(cmd == message.payload for cmd in available_commands)):
-        print('its true')
+        if DEBUG: print('DEBUG','its true')
         servetor_ai(userdata.get('gates'),message.payload)
         
 def state_checker(clnt,gates):
@@ -82,7 +82,7 @@ def state_checker(clnt,gates):
     #3 - k8 - M2 - closing 
     #4 - l6 - Limit switch open 
     #5 - l7 - Limit switch close
-    print('woop')
+    if DEBUG: print('DEBUG','woop')
     #time.sleep(1)
     state_fun = gates.unknown_state(read_pin())
     state = ''
@@ -99,7 +99,7 @@ def state_checker(clnt,gates):
             pins,pin_states = filter_pins(pin_states,win,win_len,round_dig)
             state_fun = state_fun(np.rint(pins))
             state = gates.state
-            #print(gates.state,gates.prev_state)
+            if DEBUG: print('DEBUG',gates.state,gates.prev_state)
             #    with open('pin_log.csv','a') as f:
             #        writer = csv.writer(f)
             #        writer.writerow([read_pin()[0:4],time.time()])
@@ -110,7 +110,7 @@ def state_checker(clnt,gates):
                 prev_state = state
                 sys.stdout.flush()
             time.sleep(0.1)
-                    #print('boop')
+                    if DEBUG: print('DEBUG','boop')
         else:
             pin_states = np.append(pin_states,read_pin()[3])
             reads_counter+=1
@@ -126,7 +126,7 @@ def filter_pins(pin_states,win,win_len,dig):
     pin_states[-1] = read_pin()[3]
     motion_dir = np.round(smooth(pin_states,win_len,win),dig)
     motion_dir = np.array([np.mean(motion_dir)])
-    #print(motion_dir)
+    if DEBUG: print('DEBUG',motion_dir)
     motion_dir[motion_dir>0.80] = 1
     motion_dir[motion_dir<0.50] = 2
     motion_dir[motion_dir<0.75] = -1
@@ -140,14 +140,14 @@ def filter_pins(pin_states,win,win_len,dig):
         pins[0:4] = 0
     return pins,pin_states
 def nsc():
-    print('i have no idea what do you want from me')
+    if DEBUG: print('DEBUG','i have no idea what do you want from me')
 
 def servetor_ai(gate,msg):
     #new_command = 0
-    print('servetor is online')
-    print('command received')
+    if DEBUG: print('DEBUG','servetor is online')
+    if DEBUG: print('DEBUG','command received')
     servetor.get(msg,nsc)(gate) 
-    time.sleep(0.1)
+    #time.sleep(0.1)
 	
 gpio.setmode(gpio.BCM)
 gpio.setup(26,gpio.IN)
@@ -176,5 +176,5 @@ try:
         sys.stdout.flush()
 
 except KeyboardInterrupt:
-    print('exterminate')
-    print('the dead are useless')
+    if DEBUG: print('DEBUG','exterminate')
+    if DEBUG: print('DEBUG','the dead are useless')
