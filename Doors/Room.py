acdccf120b1f955aa23be287959378e2a78506b3
manornot <ormanis1609@gmail.com>
2020-05-12 10:50:41 +0300

    increased delay

--- Doors/Room.py
+++ Doors/Room.py
@@ -45,6 +45,6 @@ class Room():
                     logging.debug(f'new uid is {uid_new}')
                     if uid is None:
                         break
-                    time.sleep(0.5)
+                    time.sleep(1)
             else:
                 time.sleep(0.5)
