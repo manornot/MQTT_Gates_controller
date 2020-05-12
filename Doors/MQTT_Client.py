e4bcc2d9a122397d9a8c81ed99fc61a7b76d6a31
manornot <ormanis1609@gmail.com>
2020-05-12 13:31:01 +0300

    fix

--- Doors/MQTT_Client.py
+++ Doors/MQTT_Client.py
@@ -34,7 +34,7 @@ class MQTT_Client:
         logger.debug(f'connecting to  {self.host}')
 
     def onConnect(self, client, userdata, flags, rc):
-        self.mqtt_client.subscribe(self.command_topic)
+        client.subscribe(self.command_topic)
         logger.debug(f'subscribed to {self.command_topic}')
 
     def request_open(self, tag):
